114a342674894246ad064d560713bebd2d2dc75e
colmo0912 <colmo0912@gmail.com>
2021-05-02 11:20:03 +0200

    aggiungo v-for

---
+++ script.js
@@ -0,0 +1,23 @@
+const slider = new Vue({
+
+    el: "#app",
+    data:{
+
+        itemImg: [
+        
+        "https://picsum.photos/id/237/560/450",
+        "https://picsum.photos/id/238/560/450",
+        "https://picsum.photos/id/239/560/450",
+
+    
+    ]
+
+    },
+
+
+    methods:{
+
+
+    }
+
+})
